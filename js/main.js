29cdca018fcd43200d01dc72a468d470dc58d609
Afozez <160236243+Afozez@users.noreply.github.com>
2025-06-24 18:09:00 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 // ========================
 // SIDEBAR NAVIGASI
 // ========================
+
+// Panggil elemen html mengunakan id dan class
 const areaTriangle = document.getElementById("area-triangle");
 const circumferencetriangle = document.getElementById("circumference-triangle");
 const btnAreaTriangle = document.querySelector(".btn-area-triangle");
 const btnCircumferenceTriangle = document.querySelector(".btn-circumference-triangle");
 
+// Default
 circumferencetriangle.style.display = "none";
 btnAreaTriangle.classList.add("btn-active");
 
+// Navigasi hitung luas segitiga
 const navAreaTriangle = () => {
     areaTriangle.style.display = "block";
     circumferencetriangle.style.display = "none";
@@ -16,6 +20,7 @@ const navAreaTriangle = () => {
     btnCircumferenceTriangle.classList.remove("btn-active");
 }
 
+// Navigasi hitung keliling segitiga
 const navCircumferenceTriangle = () => {
     areaTriangle.style.display = "none";
     circumferencetriangle.style.display = "block";
@@ -28,6 +33,8 @@ const navCircumferenceTriangle = () => {
 // ========================
 // ERROR HANDLING
 // ========================
+
+// Fungsi menampilkna pesan error pada input
 const handleError = (id, message) => {
     const error = document.getElementById(`error-message-${id}`)
     error.textContent = message;
@@ -36,16 +43,19 @@ const handleError = (id, message) => {
 // ========================
 // HITUNG LUAS SEGITIGA
 // ========================
+
+// Panggil elemen html mengunakan id dan class
 const formAreaTriangle = document.getElementById("form-area-triangle");
 const base = document.getElementById("base");
 const height = document.getElementById("height");
 const resultArea = document.querySelector(".result-area");
 
 const calculateArea = () => {
-
+    // Ambil nilai dari input
     const baseValue = parseFloat(base.value);
     const heightValue = parseFloat(height.value);
 
+    // Validasi input
     if(!baseValue || !heightValue || isNaN(baseValue) || isNaN(heightValue)) {
         handleError("area", "Masukkan nilai alas dan tinggi segitiga!");
         return;
@@ -53,8 +63,10 @@ const calculateArea = () => {
         handleError("area", "");
     }
 
+    // Rumus hitung luas segitiga
     const wide = 1/2 * baseValue * heightValue;
 
+    // Tampilkan hasil
     const resultText = `
         <li>L = 1/2 x a x t</li>
         <li>L = 1/2 x ${baseValue} x ${heightValue}</li>
@@ -63,8 +75,9 @@ const calculateArea = () => {
     resultArea.innerHTML = resultText;
 }
 
+// Menambahkan event submit pada form
 formAreaTriangle.addEventListener("submit", (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Mencegah reload form
     calculateArea();
 });
 
@@ -73,6 +86,8 @@ formAreaTriangle.addEventListener("submit", (e) => {
 // ========================
 // HITUNG KELILING SEGITIGA
 // ========================
+
+// Panggil elemen html mengunakan id dan class
 const formCircumferenceTriangles = document.getElementById("form-circumference-triangle");
 const sideA = document.getElementById("side-a");
 const sideB = document.getElementById("side-b");
@@ -80,10 +95,12 @@ const sideC = document.getElementById("side-c");
 const resultCircumference = document.querySelector(".result-circumference");
 
 const calculateCircumference = () => {
+    // Ambil niali input
     const sideAValue = parseFloat(sideA.value);
     const sideBValue = parseFloat(sideB.value);
     const sideCValue = parseFloat(sideC.value);
 
+    // Validasi input
     if(!sideAValue || !sideBValue || !sideCValue || isNaN(sideAValue) || isNaN(sideBValue) || isNaN(sideCValue)) {
         handleError("circumference", "Masukkan nilai semua sisi segitiga!");
         return;
@@ -91,8 +108,10 @@ const calculateCircumference = () => {
         handleError("circumference", "");
     }
 
+    // Rumus hitung keliling segitiga
     const circumference = sideAValue + sideBValue + sideCValue;
 
+    // Tampilkan hasil
     const resultText = `
         <li>K = a + b + c</li>
         <li>K = ${sideAValue} + ${sideBValue} + ${sideCValue}</li>
@@ -101,6 +120,7 @@ const calculateCircumference = () => {
     resultCircumference.innerHTML = resultText;
 }
 
+// Menambhakn event submit pada form
 formCircumferenceTriangles.addEventListener("submit", (e) => {
     e.preventDefault();
     calculateCircumference();
